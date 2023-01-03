c9a02b75f99e47956bf94e2a58344a0267b13132
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-01-03 16:03:49 +0000

    Add each of the stages for the hangman

--- run.py
+++ run.py
@@ -5,6 +5,8 @@
 import random
 from words import word_list
 
+# python3 run.py
+
 
 def new_game():
     """
@@ -17,16 +19,102 @@ def new_game():
     print("Wrong guesses will result in the hangman being built.")
     print("Guess all correct letters of the word before he is complete!\n")
 
-    get_word()
+    get_random_word()
 
 
-def get_word():
+def get_random_word():
     """
-    Gets a random word from the word.py file
-    Assigns it to the variable word
+    Assigns a random word from the word.py file to the variable 'word'.
     """
     word = random.choice(word_list)
-    return word
+    play(word)
+
+
+def play(word):
+    """
+    Prints word_str to the user.
+    """
+    word_guess = "_" * len(word)
+    print(word_guess)
+
+
+def hangman(stage):
+    """
+    Defines each stage of the hangman as items on a list which
+    can be iterated as neccissary and displayed to the user.
+    """
+    stages = [  # 6th stage: head, body, arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \
+                   --_
+                """,
+                # 5th stage: head, body, arms, and leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   --_
+                """,
+                # 4th stage: head, body, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   --_
+                """,
+                # 3rd stage: head, body, and arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   --_
+                """,
+                # 2nd stage: head and body
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   --_
+                """,
+                # 1st stage: head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   --_
+                """,
+                # base/empty template
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   --_
+                """
+    ]
+    return stages[stage]
 
 
 new_game()
