cf842018d1f13e05318c91a6c13571801f11efb6
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-01-09 11:11:40 +0000

    Add print statement to show user the word after they have lost

--- run.py
+++ run.py
@@ -83,7 +83,8 @@ def play_game():
     if word_complete:
         print("Congratulations! You completed the word!\n")
     else:
-        print("Unlucky! You ran out of guesses.\n")
+        print("Unlucky! You ran out of guesses.")
+        print(f"The word was: {word}.\n")
 
     question()
 
